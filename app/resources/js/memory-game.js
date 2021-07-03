461a60faddb522b15fbd52ddd4d4a62b65867798
DimovDimo <d.dimov1996@gmail.com>
2021-07-03 19:22:54 +0300

    added functions createItem, setStyle & setColor

--- app/resources/js/memory-game.js
+++ app/resources/js/memory-game.js
@@ -11,7 +11,26 @@ function addItems() {
 }
 
 function addItem() {
-    let newItem = document.createElement(typeGameItem);
+    let item = createItem();
     let game = document.getElementById(gameId);
-    game.appendChild(newItem);
+    game.appendChild(item);
+}
+
+function createItem() {
+    let item = document.createElement(typeGameItem);
+    setStyle(item);
+    setColor(item);
+
+    return item;
+}
+
+function setStyle(item) {
+    item.style.margin = "10px";
+    item.style.width = "100px";
+    item.style.height = "100px";
+    item.style.display = "inline-block";
+}
+
+function setColor(item) {
+    item.style.background = "blue"; //TODO
 }
