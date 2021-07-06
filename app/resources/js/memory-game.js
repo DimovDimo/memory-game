9728b390315351815ebd702d1d133fe11c095462
DimovDimo <d.dimov1996@gmail.com>
2021-07-06 21:43:13 +0300

    added sameColors

--- app/resources/js/memory-game.js
+++ app/resources/js/memory-game.js
@@ -1,5 +1,6 @@
 const hexSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 const countItems = 9;
+const sameColors = 2;
 const typeGameItem = "div";
 const gameId = "game";
 let game = document.getElementById(gameId);
@@ -9,16 +10,26 @@ document.body.onload = addItemsTest;
 
 // TODO: Test
 function addItemsTest() {
-    colors = [];
-    for (let count = 0; count < countItems; count++) {
-        // TODO: countItems
+    let colors = [];
+    let uniqueColors = Math.ceil(countItems / sameColors);
+    for (let index = 0; index < uniqueColors; index++) {
+        let newColor = getPseudoRandomHexColorCode();
+        if (!colors.includes(newColor)) {
+            for (let indexUniqueColors = 0; indexUniqueColors < sameColors; indexUniqueColors++) {
+                colors.push(newColor);
+            }
+        } else {
+            index--;
+        }
     }
 
+    // TODO: random colors order
+
     let items = [];
-    for (let count = 0; count < countItems; count++) {
+    for (let index = 0; index < countItems; index++) {
         let item = document.createElement(typeGameItem);
         setStyle(item);
-        setColor(item);
+        setColor(item, colors[index]); // TODO: color text
         items.push(item);
     }
 
@@ -52,8 +63,8 @@ function setStyle(item) {
     item.style.display = "inline-block";
 }
 
-function setColor(item) {
-    item.style.background = getPseudoRandomHexColorCode();
+function setColor(item, color) {
+    item.style.background = color;
 }
 
 function getPseudoRandomHexColorCode() {
