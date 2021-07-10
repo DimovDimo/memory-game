a32905e3653102ac83dceb03717165c21abbf534
DimovDimo <d.dimov1996@gmail.com>
2021-07-10 20:52:05 +0300

    removed functions

--- app/resources/js/memory-game.js
+++ app/resources/js/memory-game.js
@@ -5,11 +5,9 @@ const typeGameItem = "div";
 const gameId = "game";
 let game = document.getElementById(gameId);
 
-// document.body.onload = addItems;
-document.body.onload = addItemsTest;
+document.body.onload = addItems;
 
-// TODO: Test
-function addItemsTest() {
+function addItems() {
     let colors = [];
     let uniqueColors = Math.ceil(countItems / sameColors);
     for (let index = 0; index < uniqueColors; index++) {
@@ -28,8 +26,11 @@ function addItemsTest() {
     let items = [];
     for (let index = 0; index < countItems; index++) {
         let item = document.createElement(typeGameItem);
-        setStyle(item);
-        setColor(item, colors[index]);
+        item.style.margin = "10px";
+        item.style.width = "100px";
+        item.style.height = "100px";
+        item.style.display = "inline-block";
+        item.style.background = colors[index];
         let colorName = document.createElement("span");
         colorName.innerText = colors[index];
         item.appendChild(colorName);
@@ -37,37 +38,6 @@ function addItemsTest() {
     }
 
     items.forEach(item => game.appendChild(item));
-}
-
-function addItems() {
-    for (let count = 0; count < countItems; count++) {
-        addItem();
-    }
-}
-
-function addItem() {
-    let item = createItem();
-    let game = document.getElementById(gameId);
-    game.appendChild(item);
-}
-
-function createItem() {
-    let item = document.createElement(typeGameItem);
-    setStyle(item);
-    setColor(item);
-
-    return item;
-}
-
-function setStyle(item) {
-    item.style.margin = "10px";
-    item.style.width = "100px";
-    item.style.height = "100px";
-    item.style.display = "inline-block";
-}
-
-function setColor(item, color) {
-    item.style.background = color;
 }
 
 function randomHexColorCode() {
