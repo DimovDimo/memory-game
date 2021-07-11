e7029bc92b55279bdcc47849f391f4077c7d59a8
DimovDimo <d.dimov1996@gmail.com>
2021-07-11 11:46:22 +0300

    added functions setColors & createItems

--- app/resources/js/memory-game.js
+++ app/resources/js/memory-game.js
@@ -8,36 +8,25 @@ let game = document.getElementById(gameId);
 document.body.onload = addItems;
 
 function addItems() {
+    let colors = setColors();
+    fisherYatesShuffle(colors);
+    let items = createItems(colors);
+    items.forEach(item => game.appendChild(item));
+}
+
+function setColors() {
     let colors = [];
     let uniqueColors = Math.ceil(countItems / sameColors);
     for (let index = 0; index < uniqueColors; index++) {
         let newColor = randomHexColorCode();
         if (!colors.includes(newColor)) {
-            for (let indexUniqueColors = 0; indexUniqueColors < sameColors; indexUniqueColors++) {
-                colors.push(newColor);
-            }
+            pushSameColors(colors, newColor);
         } else {
             index--;
         }
     }
 
-    fisherYatesShuffle(colors);
-
-    let items = [];
-    for (let index = 0; index < countItems; index++) {
-        let item = document.createElement(typeGameItem);
-        item.style.margin = "10px";
-        item.style.width = "100px";
-        item.style.height = "100px";
-        item.style.display = "inline-block";
-        item.style.background = colors[index];
-        let colorName = document.createElement("span");
-        colorName.innerText = colors[index];
-        item.appendChild(colorName);
-        items.push(item);
-    }
-
-    items.forEach(item => game.appendChild(item));
+    return colors;
 }
 
 function randomHexColorCode() {
@@ -57,4 +46,36 @@ function fisherYatesShuffle(arrInput) {
         arrInput[current] = arrInput[random];
         arrInput[random] = transitional;
     }
+}
+
+function pushSameColors(colors, newColor) {
+    for (let indexUniqueColors = 0; indexUniqueColors < sameColors; indexUniqueColors++) {
+        colors.push(newColor);
+    }
+}
+
+function createItems(colors) {
+    let items = [];
+    for (let index = 0; index < countItems; index++) {
+        let item = document.createElement(typeGameItem);
+        setStyle(item, colors, index);
+        setColorName(colors, index, item);
+        items.push(item);
+    }
+
+    return items;
+}
+
+function setStyle(item, colors, index) {
+    item.style.margin = "10px";
+    item.style.width = "100px";
+    item.style.height = "100px";
+    item.style.display = "inline-block";
+    item.style.background = colors[index];
+}
+
+function setColorName(colors, index, item) {
+    let colorName = document.createElement("span");
+    colorName.innerText = colors[index];
+    item.appendChild(colorName);
 }
